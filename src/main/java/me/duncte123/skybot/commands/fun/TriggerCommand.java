f23bdd43724e3e9df1060e65e7e16ef5ef266d51
Duncan Sterken <duncte123@users.noreply.github.com>
2017-07-26 09:22:21 +0200

    Update TriggerCommand

--- src/main/java/me/duncte123/skybot/commands/fun/TriggerCommand.java
+++ src/main/java/me/duncte123/skybot/commands/fun/TriggerCommand.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import me.duncte123.skybot.Command;
 import me.duncte123.skybot.utils.Config;
+import me.duncte123.skybot.utils.Functions;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
@@ -19,13 +20,7 @@ public class TriggerCommand implements Command {
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
-        EmbedBuilder eb = new EmbedBuilder()
-                .setColor(Config.defaultColour)
-                .setAuthor(Config.headerName, Config.defaultUrl, Config.defaultIcon)
-                .setImage("https://cdn.discordapp.com/attachments/94831883505905664/176181155467493377/triggered.gif")
-                .setFooter(Config.defaultName, Config.defaultIcon)
-                .setTimestamp(Instant.now());
-                event.getTextChannel().sendMessage(eb.build()).queue();
+                event.getTextChannel().sendMessage(Functions.embedImage("https://cdn.discordapp.com/attachments/94831883505905664/176181155467493377/triggered.gif")).queue();
     }
 
     @Override
