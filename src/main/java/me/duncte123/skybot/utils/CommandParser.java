c07a3e38fd461663164338a2355689d53626f4cc
Duncan Sterken <duncte123@users.noreply.github.com>
2017-07-28 11:10:11 +0200

    Optimise code

--- src/main/java/me/duncte123/skybot/utils/CommandParser.java
+++ src/main/java/me/duncte123/skybot/utils/CommandParser.java
@@ -1,6 +1,7 @@
 package me.duncte123.skybot.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
@@ -8,30 +9,19 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class CommandParser {
 
     public CommandContainer parse(String rw, MessageReceivedEvent e){
-        ArrayList<String> split = new ArrayList<String>();
-        String raw = rw;
-        String beheaded = raw.replaceFirst(Config.prefix, "");
-        String[] SplitBeheadded = beheaded.split(" ");
-        for(String s : SplitBeheadded){split.add(s);}
-        String invoke = split.get(0).toLowerCase();
-        String[] args = new String[split.size() - 1];
-        split.subList(1, split.size()).toArray(args);
+        final String[] split = rw.substring(rw.indexOf(Config.prefix) + 1, rw.length()).split(" ");
+        final String invoke = split[0];
+        final String[] args = Arrays.copyOfRange(split, 1, split.length);
 
-        return new CommandContainer(raw, beheaded, SplitBeheadded, invoke, args, e);
+        return new CommandContainer(invoke, args, e);
     }
 
      public class CommandContainer {
-         public final String raw;
-         public final String beheaded;
-         public final String[] SplitBeheadded;
          public final String invoke;
          public final String[] args;
          public final MessageReceivedEvent event;
 
-         public CommandContainer(String rw, String beheaded, String[] SplitBeheaded, String invoke, String[] args, MessageReceivedEvent e){
-             this.raw = rw;
-             this.beheaded = beheaded;
-             this.SplitBeheadded = SplitBeheaded;
+         public CommandContainer(String invoke, String[] args, MessageReceivedEvent e){
              this.invoke = invoke;
              this.args = args;
              this.event = e;
