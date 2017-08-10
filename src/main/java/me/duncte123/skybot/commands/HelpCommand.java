cf7ee3fe96d8a8a106883b12de7208489c7034b5
Duncan Sterken <duncte123@users.noreply.github.com>
2017-08-10 19:20:48 +0200

    Create failover for if the helpmessage fails to send

--- src/main/java/me/duncte123/skybot/commands/HelpCommand.java
+++ src/main/java/me/duncte123/skybot/commands/HelpCommand.java
@@ -2,6 +2,7 @@ package me.duncte123.skybot.commands;
 
 import me.duncte123.skybot.Command;
 import me.duncte123.skybot.utils.HelpEmbeds;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class HelpCommand extends Command {
@@ -19,13 +20,25 @@ public class HelpCommand extends Command {
 
 
         event.getAuthor().openPrivateChannel().queue( (pc) -> {
-            event.getTextChannel().sendMessage(event.getMember().getAsMention() +" check your DM's").queue();
-            pc.sendMessage(HelpEmbeds.mainCommands).queue();
-            pc.sendMessage(HelpEmbeds.musicCommands).queue();
-            pc.sendMessage(HelpEmbeds.funCommands).queue();
-            pc.sendMessage(HelpEmbeds.modCommands).queue();
+            pc.sendMessage(HelpEmbeds.mainCommands).queue( msg -> {
+                        pc.sendMessage(HelpEmbeds.musicCommands).queue();
+                        pc.sendMessage(HelpEmbeds.funCommands).queue();
+                        pc.sendMessage(HelpEmbeds.modCommands).queue();
+                        event.getTextChannel().sendMessage(event.getMember().getAsMention() +" check your DM's").queue();
+                    },
+                    //When sending fails, send to the channel
+                    err -> {
+                        TextChannel currentChann = event.getTextChannel();
+                        currentChann.sendMessage(HelpEmbeds.mainCommands).queue();
+                        currentChann.sendMessage(HelpEmbeds.musicCommands).queue();
+                        currentChann.sendMessage(HelpEmbeds.funCommands).queue();
+                        currentChann.sendMessage(HelpEmbeds.modCommands).queue();
+                        currentChann.sendMessage("Message could not be delivered to dm's and has been send in this channel.").queue();
+                    }
+            );
         },
-                err -> event.getChannel().sendMessage("ERROR: " + err.getMessage()).queue() );
+                err -> event.getChannel().sendMessage("ERROR: " + err.getMessage()).queue()
+        );
     }
 
     @Override
