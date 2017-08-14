cf9098f51c25172d4103a88ab27a08223c1f3170
Duncan Sterken <duncte123@users.noreply.github.com>
2017-08-14 16:15:00 +0200

    Wrong prefix

--- src/main/java/me/duncte123/skybot/utils/Config.java
+++ src/main/java/me/duncte123/skybot/utils/Config.java
@@ -10,7 +10,7 @@ public class Config {
     //The api key for http://thecatapi.com/, optional
     public static String catAPIKey = "";
     // This is the prefix that your bot has, by default is the /
-    public static final String prefix = "!";
+    public static final String prefix = "/";
     // This is the version of the bot
     public static final String version = "3.33.0";
     // This is the name that your bot has
