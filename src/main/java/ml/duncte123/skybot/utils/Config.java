f87ea7c8223ca8567af913a063c490842b4fca4a
Duncan Sterken <duncte123@users.noreply.github.com>
2017-08-23 11:04:43 +0200

    Change prefix

--- src/main/java/ml/duncte123/skybot/utils/Config.java
+++ src/main/java/ml/duncte123/skybot/utils/Config.java
@@ -12,7 +12,7 @@ public class Config {
     //The api key for http://thecatapi.com/, optional
     public static String catAPIKey = "";
     // This is the prefix that your bot has, by default is the /
-    public static final String prefix = "!";
+    public static final String prefix = "/";
     // This is the version of the bot
     public static final String version = "3.38.0";
     // This is the name that your bot has
