1d5ace73c30660176bfaa774c520c66d42b55fab
Duncan Sterken <duncan.sterken123@gmail.com>
2017-10-12 12:00:55 +0200

    Add correct filename to the cat images from KittyCommand

--- src/main/java/ml/duncte123/skybot/commands/animals/KittyCommand.java
+++ src/main/java/ml/duncte123/skybot/commands/animals/KittyCommand.java
@@ -22,8 +22,12 @@ public class KittyCommand extends Command {
             Document raw = Jsoup.connect("http://thecatapi.com/api/images/get?" +
                     (!apiKey.isEmpty()? "api_key=" + apiKey + "&" : "") + "format=xml&results_per_page=1").get();
             Document doc = Jsoup.parse(raw.getAllElements().html(), "", Parser.xmlParser());
-            event.getChannel().sendFile(new URL(doc.select("url").first().text()).openStream(),
-                    "Kitty_" + System.currentTimeMillis() + ".png", null).queue();
+
+            String fullUrl = doc.select("url").first().text();
+            String fileName = fullUrl.split("/")[3];
+
+            event.getChannel().sendFile(new URL(fullUrl).openStream(),
+                    fileName, null).queue();
         }
         catch (Exception e) {
             sendEmbed(event, EmbedUtils.embedMessage("ERROR: " + e.getCause().toString()));
