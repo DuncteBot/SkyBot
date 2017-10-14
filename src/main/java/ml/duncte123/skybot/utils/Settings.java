637523b1e75e9cd301153db402ce51e51f477089
Duncan Sterken <duncan.sterken123@gmail.com>
2017-10-14 13:38:18 +0200

    Finish open eval

--- src/main/java/ml/duncte123/skybot/utils/Settings.java
+++ src/main/java/ml/duncte123/skybot/utils/Settings.java
@@ -18,7 +18,7 @@ public class Settings {
     /**
      * This is the version of the bot
      */
-    public static final String version = "3.48.2";
+    public static final String version = "3.48.3";
     /**
      * This is the name that your bot has
      */
