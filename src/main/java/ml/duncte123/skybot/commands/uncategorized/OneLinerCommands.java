3d9625170d5977534237331359e1ca88e41cf94e
Duncan Sterken <duncan.sterken123@gmail.com>
2017-10-31 11:58:35 +0100

    Add uptime command

--- src/main/java/ml/duncte123/skybot/commands/uncategorized/OneLinerCommands.java
+++ src/main/java/ml/duncte123/skybot/commands/uncategorized/OneLinerCommands.java
@@ -19,9 +19,12 @@
 package ml.duncte123.skybot.commands.uncategorized;
 
 import ml.duncte123.skybot.objects.command.Command;
+import ml.duncte123.skybot.utils.AirUtils;
 import ml.duncte123.skybot.utils.EmbedUtils;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import java.lang.management.ManagementFactory;
 
 /**
  * This class contains a bunch of commands that require little code to run, so we combine in this class
@@ -65,6 +68,9 @@ public class OneLinerCommands extends Command {
                 sendMsg(event, "Invite me with this link:\n" +
                         "<https://discordapp.com/oauth2/authorize?client_id=210363111729790977&scope=bot&permissions=8>");
                 break;
+            case "uptime":
+                sendMsg(event, AirUtils.getUptime(ManagementFactory.getRuntimeMXBean().getUptime(), true));
+                break;
         }
 
     }
@@ -76,7 +82,8 @@ public class OneLinerCommands extends Command {
                 "`"+this.PREFIX+"trigger` => use when you are triggered.\n" +
                 "`"+this.PREFIX+"wam` => you need more WAM!.\n" +
                 "`"+this.PREFIX+"mineh` => HERE COMES MINEH!\n" +
-                "`"+this.PREFIX+"invite` => gives you the bot invite";
+                "`"+this.PREFIX+"invite` => gives you the bot invite\n" +
+                "`"+this.PREFIX+"uptime` => shows the bot uptime";
     }
 
     @Override
@@ -86,6 +93,6 @@ public class OneLinerCommands extends Command {
 
     @Override
     public String[] getAliases() {
-        return new String[]{"cookie", "trigger", "wam", "mineh", "invite"};
+        return new String[]{"cookie", "trigger", "wam", "mineh", "invite", "uptime"};
     }
 }
