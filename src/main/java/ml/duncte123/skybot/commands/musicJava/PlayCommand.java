0b42877a5598fcaf65c08fd94811482ccfa817ac
Duncan Sterken <duncan.sterken123@gmail.com>
2017-11-15 21:02:16 +0100

    First command done

--- src/main/java/ml/duncte123/skybot/commands/musicJava/PlayCommand.java
+++ src/main/java/ml/duncte123/skybot/commands/musicJava/PlayCommand.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package ml.duncte123.skybot.commands.music;
+package ml.duncte123.skybot.commands.musicJava;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import ml.duncte123.skybot.audio.GuildMusicManager;
