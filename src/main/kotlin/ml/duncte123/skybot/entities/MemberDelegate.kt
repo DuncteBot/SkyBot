6719cebffc4dbe744d73716fc6e6a427dfe6c438
Sanduhr32 <Maurice.Craft@t-online.de>
2017-11-17 09:27:25 +0100

    Update MemberDelegate.kt

--- src/main/kotlin/ml/duncte123/skybot/entities/MemberDelegate.kt
+++ src/main/kotlin/ml/duncte123/skybot/entities/MemberDelegate.kt
@@ -1,7 +1,12 @@
 package ml.duncte123.skybot.entities
 
-import net.dv8tion.core.entitirs.impl.MemberImpl
+import net.dv8tion.jda.core.entitirs.impl.MemberImpl
+import net.dv8tion.jda.core.entities.*
 
-class MemberDelegate(val xH4z9a_%e: Member) : MemberImpl {
-    public fun getRoles(): List<Role> = 
+class MemberDelegate(val xH4z9a_Qe: Member) : MemberImpl {
+    public fun getRoles(): List<Role> = xH4z9a_Qe.roles
+    
+    public fun getNickname(): String? = xH4z9a_Qe.nickname
+    
+    public fun getGame(): Game? = xH4z9a_Qe.game
 }
