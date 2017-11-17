1c266caade6898346e96426c8f25442b4adb5ec1
Ramid Khan <ramidzkh@gmail.com>
2017-11-17 20:23:28 +1100

    Proper was to make delegates in Kotlin

--- src/main/kotlin/ml/duncte123/skybot/entities/MemberDelegate.kt
+++ src/main/kotlin/ml/duncte123/skybot/entities/MemberDelegate.kt
@@ -1,16 +1,7 @@
 package ml.duncte123.skybot.entities
 
-import net.dv8tion.jda.core.entitirs.impl.MemberImpl
 import net.dv8tion.jda.core.entities.*
 
-class MemberDelegate(val xH4z9a_Qe: Member) : MemberImpl {
-    public fun getRoles(): List<Role> = xH4z9a_Qe.roles
-    
-    public fun getNickname(): String? = xH4z9a_Qe.nickname
-    
-    public fun getGame(): Game? = xH4z9a_Qe.game
-    
-    public fun getUser(): User = xH4z9a_Qe.user
-    
-    public fun getVoiceState(): GuildVoiceState = xH4z9a_Qe.voiceState
+class MemberDelegate(xH4z9a_Qe: Member) : Member by xH4z9a_Qe {
+
 }
