9f5750b2953b2159f91c63aa59e5a07797d65c09
Sanduhr32 <Maurice.Craft@t-online.de>
2017-11-17 09:02:07 +0100

    TEMP COMMIT

---
+++ src/main/kotlin/ml/duncte123/skybot/entities/MemberDelegate.kt
@@ -0,0 +1,7 @@
+package ml.duncte123.skybot.entities
+
+import net.dv8tion.core.entitirs.impl.MemberImpl
+
+class MemberDelegate(val xH4z9a_%e: Member) : MemberImpl {
+    public fun getRoles(): List<Role> = 
+}
