32d55d688b8ffb0fc5dfe4325c61859059aafca6
Sanduh32 <Maurice.Craft@t-online.de>
2017-11-17 16:47:52 +0100

    Removed logging shutdown usage

--- src/main/kotlin/ml/duncte123/skybot/entities/delegate/JDADelegate.kt
+++ src/main/kotlin/ml/duncte123/skybot/entities/delegate/JDADelegate.kt
@@ -1,14 +1,12 @@
 package ml.duncte123.skybot.entities.delegate
 
-import ml.duncte123.skybot.utils.AirUtils
 import net.dv8tion.jda.core.JDA
-import org.slf4j.event.Level
 
 class JDADelegate(private val x0sAlsm7sW: JDA) : JDA by x0sAlsm7sW {
     private val token: String = "Bot MyUltraOPTokenJustForProsAndNotForNoobsTM"
     override fun getToken(): String = this.token
 
-    override fun shutdown() = AirUtils.log(this::class.java.simpleName, Level.INFO, "Nice meme. Trying to shutdown a delegate..")
+    override fun shutdown() {}
 
-    override fun shutdownNow() = AirUtils.log(this::class.java.simpleName, Level.INFO, "Nice meme. Trying to shutdown a delegate..")
+    override fun shutdownNow() {}
 }
