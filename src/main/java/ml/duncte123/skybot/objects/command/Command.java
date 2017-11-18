09c3d657c3f06f2ea1daa595dea877ace3cd44d9
Ramid Khan <ramidzkh@gmail.com>
2017-11-18 17:25:54 +1100

    Add upvoted feature to Command

--- src/main/java/ml/duncte123/skybot/objects/command/Command.java
+++ src/main/java/ml/duncte123/skybot/objects/command/Command.java
@@ -18,7 +18,11 @@
 
 package ml.duncte123.skybot.objects.command;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import ml.duncte123.skybot.objects.guild.GuildSettings;
+import ml.duncte123.skybot.utils.AirUtils;
 import ml.duncte123.skybot.utils.EmbedUtils;
 import ml.duncte123.skybot.utils.GuildSettingsUtils;
 import ml.duncte123.skybot.utils.Settings;
@@ -28,9 +32,21 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Command {
-
+    
+    protected static final Set<String> upvotedIds = new HashSet<>();
+    
+    static {
+        reloadUpvoted();
+    }
+    
     /**
      * This holds the prefix for us
      */
@@ -39,113 +55,153 @@ public abstract class Command {
      * This holds the category
      */
     protected CommandCategory category = CommandCategory.MAIN;
-
+    
+    protected static void reloadUpvoted() {
+        try {
+            String token = AirUtils.config.getString("apis.discordbots_userToken");
+            
+            if (token == null) {
+                AirUtils.logger.warn("Discord Bots token not found");
+                return;
+            }
+            
+            Response response = new OkHttpClient()
+                                        .newCall(
+                                                new Request.Builder()
+                                                        .url("https://discordbots.org/api/bots/210363111729790977/votes?onlyids=1")
+                                                        .get()
+                                                        .addHeader("Authorization", token)
+                                                        .build())
+                                        .execute();
+            JsonArray json = new JsonParser().parse(response.body().source().readUtf8()).getAsJsonArray();
+            
+            upvotedIds.clear();
+            
+            for (JsonElement je : json)
+                upvotedIds.add(je.getAsString());
+        } catch (Exception e) {
+            AirUtils.logger.warn("Error (re)loading upvoted people: " + e.getMessage(), e);
+        }
+    }
+    
     /**
      * Returns the current category of the command
+     *
      * @return the current category of the command
      */
     public CommandCategory getCategory() {
         return this.category;
     }
-
+    
     /**
      * This is the action of the command, this will hold what the commands needs to to
+     *
      * @param invoke The command that is ran
-     * @param args The command agruments
-     * @param event a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param args   The command agruments
+     * @param event  a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
      */
     public abstract void executeCommand(String invoke, String[] args, GuildMessageReceivedEvent event);
-
+    
     /**
      * The usage instructions of the command
+     *
      * @return a String
      */
     public abstract String help();
-
+    
     /**
      * This will hold the command name aka what the user puts after the prefix
+     *
      * @return The command name
      */
     public abstract String getName();
-
+    
     /**
      * This wil hold any aliases that this command might have
+     *
      * @return the current aliases for the command if set
      */
     public String[] getAliases() {
         return new String[0];
     }
-
+    
     /**
      * This returns the settings for the given guild
+     *
      * @param guild the guild that we need the settings for
      * @return the {@link GuildSettings GuildSettings} for the given guild
      */
     protected GuildSettings getSettings(Guild guild) {
         return GuildSettingsUtils.getGuild(guild);
     }
-
+    
     /**
      * This will react with a ❌ if the user doesn't have permission to run the command
+     *
      * @param message the message to add the reaction to
      */
     protected void sendError(Message message) {
         message.addReaction("❌").queue();
     }
-
+    
     /**
      * This will react with a ✅ if the user doesn't have permission to run the command
+     *
      * @param message the message to add the reaction to
      */
     protected void sendSuccess(Message message) {
         message.addReaction("✅").queue();
     }
-
+    
     /**
      * This will chcek if we can send a embed and convert it to a message if we can't send embeds
+     *
      * @param event a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
      * @param embed The embed to send
      */
     protected void sendEmbed(GuildMessageReceivedEvent event, MessageEmbed embed) {
-        if(!event.getGuild().getSelfMember().hasPermission(event.getChannel(), Permission.MESSAGE_EMBED_LINKS)) {
+        if (!event.getGuild().getSelfMember().hasPermission(event.getChannel(), Permission.MESSAGE_EMBED_LINKS)) {
             sendMsg(event, EmbedUtils.embedToMessage(embed));
             return;
         }
         sendMsg(event, embed);
     }
-
+    
     /**
      * This is a shortcut for sending messages to a channel
+     *
      * @param event a instance of {@link net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent GuildMessageReceivedEvent}
-     * @param msg the message to send
+     * @param msg   the message to send
      */
     protected void sendMsg(GuildMessageReceivedEvent event, String msg) {
-       sendMsg(event, (new MessageBuilder()).append(msg).build());
+        sendMsg(event, (new MessageBuilder()).append(msg).build());
     }
-
+    
     /**
      * This is a shortcut for sending messages to a channel
+     *
      * @param event a instance of {@link net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent GuildMessageReceivedEvent}
-     * @param msg the message to send
+     * @param msg   the message to send
      */
     protected void sendMsg(GuildMessageReceivedEvent event, MessageEmbed msg) {
         sendMsg(event, (new MessageBuilder()).setEmbed(msg).build());
     }
-
+    
     /**
      * This is a shortcut for sending messages to a channel
+     *
      * @param event a instance of {@link net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent GuildMessageReceivedEvent}
-     * @param msg the message to send
+     * @param msg   the message to send
      */
     protected void sendMsg(GuildMessageReceivedEvent event, Message msg) {
         event.getChannel().sendMessage(msg).queue();
     }
-
+    
     @Override
     public String toString() {
         return "Command[" + getName() + "]";
     }
-
+    
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -154,9 +210,9 @@ public abstract class Command {
         if (obj.getClass() != this.getClass()) {
             return false;
         }
-
+        
         Command command = (Command) obj;
-
+        
         return this.help().equals(command.help()) && this.getName().equals(command.getName());
     }
 }
