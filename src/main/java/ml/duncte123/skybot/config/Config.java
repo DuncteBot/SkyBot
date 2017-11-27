1f9c4d50efc5f8ce3a303a4aea8e1e3bd6d98da6
Duncan Sterken <duncan.sterken123@gmail.com>
2017-11-27 20:15:46 +0100

    Revert config back to gson (will fix org.json soon)

--- src/main/java/ml/duncte123/skybot/config/Config.java
+++ src/main/java/ml/duncte123/skybot/config/Config.java
@@ -14,28 +14,27 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 package ml.duncte123.skybot.config;
 
-import ml.duncte123.skybot.utils.AirUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.slf4j.event.Level;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 import java.io.File;
 
 public class Config {
-    
-    protected JSONObject config;
+
+    protected final JsonObject config;
     private final Config parent;
-    
-    protected Config(Config parent, JSONObject config) {
+
+    protected Config(Config parent, JsonObject config) {
         this.parent = parent;
         this.config = config;
     }
-    
+
     /**
      * idk
      *
@@ -47,7 +46,7 @@ public class Config {
     public static String replaceLast(String text, String regex, String replacement) {
         return text.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
     }
-    
+
     /**
      * This will try to get data from the config file
      *
@@ -55,10 +54,9 @@ public class Config {
      * @return the value of the setting
      */
     public String getString(String key) {
-        Object out = this.get(this.config, key);
-        return (String) out;
+        return this.getJsonPrimitive(key).getAsString();
     }
-    
+
     /**
      * This will try to get data from the config file
      *
@@ -67,10 +65,16 @@ public class Config {
      * @return the value of the setting
      */
     public final String getString(String key, String defaultValue) {
-        Object out = this.get(this.config, key);
-        return (out == null) ? defaultValue : (String) out;
+        if (!this.hasKey(key)) {
+            this.put(key, defaultValue);
+        }
+        try {
+            return this.getString(key);
+        } catch (Exception e) {
+            return defaultValue;
+        }
     }
-    
+
     /**
      * This will attempt to get an integer from the config file
      *
@@ -79,10 +83,13 @@ public class Config {
      * @throws NumberFormatException if the returned value isn't valis
      */
     public int getInt(String key) throws NumberFormatException {
-        Object out = this.get(this.config, key);
-        return (int) out;
+        try {
+            return this.getJsonPrimitive(key).getAsInt();
+        } catch (final NumberFormatException e) {
+            throw e;
+        }
     }
-    
+
     /**
      * This will attempt to get an integer from the config file
      *
@@ -91,10 +98,11 @@ public class Config {
      * @return the int
      */
     public final int getInt(String key, int defaultValue) {
-        Object out = this.get(this.config, key);
-        return (out == null) ? defaultValue : (int) out;
+        if (!this.hasKey(key))
+            this.put(key, defaultValue);
+        return this.getInt(key);
     }
-    
+
     /**
      * This will attempt to get a boolean from the config file
      *
@@ -102,10 +110,14 @@ public class Config {
      * @return the boolean from the key
      */
     public boolean getBoolean(String key) {
-        Object out = this.get(this.config, key);
-        return (boolean) out;
+        try {
+            return this.getJsonPrimitive(key).getAsBoolean();
+        } catch (final Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-    
+
     /**
      * This will attempt to get a boolean from the config file
      *
@@ -114,71 +126,174 @@ public class Config {
      * @return the boolean from the key
      */
     public final boolean getBoolean(String key, boolean defaultValue) {
-
-        Object out = this.get(this.config, key);
-        return (out == null) ? defaultValue : (boolean) out;
+        if (!this.hasKey(key))
+            this.put(key, defaultValue);
+        return this.getBoolean(key);
     }
 
-    public void put(String key, Object value) {
+    /**
+     * This will load from our config with the key
+     *
+     * @param key the key to find
+     * @return this thing called {@link com.google.gson.JsonPrimitive JsonPrimitive}
+     * @throws NullPointerException when the key is not found
+     */
+    public JsonPrimitive getJsonPrimitive(String key) throws NullPointerException {
+        return this.getJsonElement(key).getAsJsonPrimitive();
+    }
+
+    /**
+     * This will load from our config with the key
+     *
+     * @param key the key to find
+     * @return a nice JsonElement
+     * @throws NullPointerException When things are about too go down
+     */
+    public JsonElement getJsonElement(String key) throws NullPointerException {
         final String[] path = key.split("\\.");
+        JsonElement value = this.config;
+        try {
+            for (String element : path) {
+                // System.out.println(element);
+                if (element.trim().isEmpty())
+                    continue;
+                if (element.endsWith("]") && element.contains("[")) {
+                    final int i = element.lastIndexOf("[");
+                    int index;
+                    try {
+                        index = Integer.parseInt(element.substring(i).replace("[", "").replace("]", ""));
+                    } catch (final Exception e) {
+                        index = 0;
+                    }
+                    element = element.substring(0, i);
+
+                    value = value.getAsJsonObject().get(element);
+                    value = value.getAsJsonArray().get(index);
+
+                } else
+                    value = value.getAsJsonObject().get(element);
+            }
+            if (value == null)
+                throw new NullPointerException("Key '" + key + "' has no value or doesn't exists, trying to add it");
+            return value;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * This will check if the key that we are looking for
+     *
+     * @param key the key to find
+     * @return true if the key is there
+     */
+    public boolean hasKey(String key) {
+        try {
+            return this.getJsonElement(key) != null;
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
+    /**
+     * This will attempt to put a value is the config
+     *
+     * @param key   the key to add the value under
+     * @param value the value that we need to add, in the form of an {@link com.google.gson.JsonElement JsonElement}
+     * @throws Exception when we fail
+     */
+    public void put(String key, JsonElement value) throws Exception {
+        final String finalKey = key.substring(key.lastIndexOf(".") + 1);
+        key = replaceLast(key, finalKey, "");
+        if (key.endsWith("."))
+            key = replaceLast(key, ".", "");
+        final String[] path = key.split("\\.");
+        JsonObject current = this.config;
 
         try {
-            JSONObject temp = this.config;
-            for (int i = 0; i < path.length; i++) {
-                if (i == path.length - 1) {
-                    temp.put(path[i], value);
-                    break;
-                }
-                Object x = this.get(temp, path[i]);
-                if (x == null) {
-                    if (path.length == 1) {
-                        this.config.put(key, value);
-                    } else {
-                        AirUtils.log(Level.ERROR, "Lol is this possible?");
+            for (String element : path) {
+                if (element.trim().isEmpty())
+                    continue;
+                if (element.endsWith("]") && element.contains("[")) {
+                    final int i = element.lastIndexOf("[");
+                    int index;
+                    try {
+                        index = Integer.parseInt(element.substring(i).replace("[", "").replace("]", ""));
+                    } catch (final Exception e) {
+                        index = -1;
                     }
+                    element = element.substring(0, i);
+
+                    if (!current.has(element))
+                        current.add(element, new JsonArray());
+                    final JsonArray array = current.get(element).getAsJsonArray();
+                    if (index == -1) {
+                        final JsonObject object = new JsonObject();
+                        array.add(object);
+                        current = object;
+                    } else {
+                        if (index == array.size())
+                            array.add(new JsonObject());
+                        current = array.get(index).getAsJsonObject();
+                    }
+
                 } else {
-                    temp = new JSONObject(x.toString());
-                    temp = new JSONObject().put(path[i], temp);
+                    if (!current.has(element))
+                        current.add(element, new JsonObject());
+                    current = current.get(element).getAsJsonObject();
                 }
             }
-            this.config = temp;
-        } catch (JSONException ex) {
-            AirUtils.log(Level.ERROR, ex.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
         }
+        current.add(finalKey, value);
+        this.save();
+    }
 
+    /**
+     * This will attempt to put a value is the config
+     *
+     * @param key   the key to add the value under
+     * @param value the value that we need to add
+     */
+    public void put(String key, String value) {
         try {
-            this.save();
+            this.put(key, new JsonPrimitive(value));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * This methods gets an item of an JSON really quick but
-     * can't be used for putting items!
-     * @param jsonData the json where it should search
-     * @param key key aka path in the json
-     * @return Object probably a json or any other..
+     * This will attempt to put a value is the config
+     *
+     * @param key   the key to add the value under
+     * @param value the value that we need to add
      */
-    private Object get(JSONObject jsonData, String key) {
-        if (key.isEmpty())
-            return null;
-        final String[] path = key.split("\\.");
-        JSONObject current = jsonData;
-            for (int i = 0; i < path.length; i++) {
-                try {
-                    if (jsonData.isNull(path[i]))
-                        jsonData.put(path[i], new Object());
-                    if (i == path.length - 1)
-                        return current.get(path[i]);
-                    current = new JSONObject(current.get(path[i]).toString());
-                } catch (JSONException ex) {
-                    AirUtils.log(Level.ERROR, ex.toString());
-                }
-            }
-        return null;
+    public void put(String key, Number value) {
+        try {
+            this.put(key, new JsonPrimitive(value));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
+    /**
+     * This will attempt to put a value is the config
+     *
+     * @param key   the key to add the value under
+     * @param value the value that we need to add
+     */
+    public void put(String key, boolean value) {
+        try {
+            this.put(key, new JsonPrimitive(value));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * get the config as a file
      *
@@ -187,7 +302,7 @@ public class Config {
     public File getConfigFile() {
         return this.parent.getConfigFile();
     }
-    
+
     /**
      * save the config
      *
--- src/main/java/ml/duncte123/skybot/config/ConfigLoader.java
+++ src/main/java/ml/duncte123/skybot/config/ConfigLoader.java
@@ -14,17 +14,16 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 package ml.duncte123.skybot.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 import org.apache.commons.text.translate.UnicodeUnescaper;
-import org.json.JSONObject;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.util.stream.Collectors;
 
 public class ConfigLoader {
 
@@ -46,29 +45,36 @@ public class ConfigLoader {
     }
 
     public static class MainConfig extends Config {
-    
+
         private final File configFile;
-    
+
         MainConfig(final File file) throws Exception {
-            super(null, new JSONObject(Files.readAllLines(file.toPath()).stream().filter(line -> !line.trim().startsWith("//")).collect(Collectors.joining())));
+            super(null, new JsonParser().parse(new FileReader(file)).getAsJsonObject());
             this.configFile = file;
         }
-    
+
         @Override
         public File getConfigFile() {
             return this.configFile;
         }
-    
+
         @Override
         public void save() throws Exception {
-            final String json = this.config.toString();
+            final Gson gson = new GsonBuilder()
+                    .serializeNulls()
+                    .setPrettyPrinting()
+                    .serializeSpecialFloatingPointValues()
+                    .create();
+            final String json = gson.toJson(this.config);
             try {
-                final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.configFile), "UTF-8"));
+                final BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(new FileOutputStream(this.configFile), "UTF-8"));
                 new UnicodeUnescaper().translate(json, writer);
                 writer.close();
             } catch (final IOException e) {
                 e.printStackTrace();
             }
         }
+
     }
 }
