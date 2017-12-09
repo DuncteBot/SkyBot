93415852701d7e5d1d07227cb14d55d6c5802c06
Sanduhr32 <Maurice.Craft@t-online.de>
2017-12-09 21:41:55 +0100

    Memes
    Since IntelliJ doesnt support commiting UTF-8 char name files i have to upload them manually

---
+++ src/main/kotlin/ml/duncte123/skybot/(╯°□°）╯︵ ┻━┻Exception.kt
@@ -0,0 +1,34 @@
+/*
+ * Skybot, a multipurpose discord bot
+ *      Copyright (C) 2017  Duncan "duncte123" Sterken & Ramid "ramidzkh" Khan & Maurice R S "Sanduhr32"
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@file:Author(nickname = "Sanduhr32", author = "Maurice R S")
+
+package ml.duncte123.skybot
+
+@Author(nickname = "Sanduhr32", author = "Maurice R S")
+class `(╯°□°）╯︵ ┻━┻Exception`() : Exception() /* This might be an VRCubeException */{
+
+    override lateinit var message: String
+
+
+    fun getMeme(): String = "(╯°□°）╯︵ ┻━┻"
+
+    constructor(message: String) : this() {
+        this.message = message
+    }
+}
