6af0ae586343c272604f42a32ada8c5ac239fb6d
Sanduhr32 <Maurice.Craft@t-online.de>
2017-12-10 12:59:35 +0100

    Update JDADelegate.kt

--- src/main/kotlin/ml/duncte123/skybot/entities/delegate/JDADelegate.kt
+++ src/main/kotlin/ml/duncte123/skybot/entities/delegate/JDADelegate.kt
@@ -36,7 +36,7 @@ class JDADelegate(private val x0sAlsm7sW: JDA) : JDA by x0sAlsm7sW {
     
     override fun getToken(): String      = this.token
     override fun getPresence(): Presence = this.presence
-    override fun shutdown()              = throw TFException("Can not shutdown")
+    override fun shutdown()              = throw Exception("Can not shutdown")
     override fun shutdownNow()           = this.shutdown()
     
     override fun equals(other: Any?): Boolean {
