c15545b174de2a85af14025f6a4c9b0fa4af7c2b
Duncan Sterken <duncan.sterken123@gmail.com>
2017-12-11 15:22:53 +0100

    Set botowner id in config

--- src/main/java/ml/duncte123/skybot/utils/Settings.java
+++ src/main/java/ml/duncte123/skybot/utils/Settings.java
@@ -26,7 +26,7 @@ public class Settings {
     /**
      * The userID from the guy that is hosting the bot, in most cases that is just my id :D
      */
-    public static final String ownerId = BuildConfig.ownerId;
+    public static final String ownerId = AirUtils.config.getString("discord.botOwnerId", BuildConfig.ownerId);
 
     /**
      * This contains a list of different id's
