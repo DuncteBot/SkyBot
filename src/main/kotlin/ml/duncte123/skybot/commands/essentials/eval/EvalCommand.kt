3739ead3c7d07b4b3f54e2b61347b004324a0399
Sanduh32 <Maurice.Craft@t-online.de>
2018-01-23 20:13:00 +0100

    [Fix] eval class file imports

--- src/main/kotlin/ml/duncte123/skybot/commands/essentials/eval/EvalCommand.kt
+++ src/main/kotlin/ml/duncte123/skybot/commands/essentials/eval/EvalCommand.kt
@@ -21,13 +21,13 @@ package ml.duncte123.skybot.commands.essentials.eval
 import Java.lang.VRCubeException
 import groovy.lang.GroovyShell
 import kotlinx.coroutines.experimental.*
+import ml.duncte123.skybot.Settings
 import ml.duncte123.skybot.SinceSkybot
 import ml.duncte123.skybot.commands.essentials.eval.filter.EvalFilter
 import ml.duncte123.skybot.entities.delegate.*
 import ml.duncte123.skybot.objects.command.Command
 import ml.duncte123.skybot.objects.command.CommandCategory
 import ml.duncte123.skybot.utils.AirUtils
-import ml.duncte123.skybot.Settings
 import ml.duncte123.skybot.utils.TextColor
 import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
