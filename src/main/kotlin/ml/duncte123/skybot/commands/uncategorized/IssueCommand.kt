53fc6ee4e02dc0fa83cbe0f368f9dfa0dcbd3e14
Sanduh32 <Maurice.Craft@t-online.de>
2018-03-16 10:21:00 +0100

    Fuck you IntelliJ for adding it.

--- src/main/kotlin/ml/duncte123/skybot/commands/uncategorized/IssueCommand.kt
+++ src/main/kotlin/ml/duncte123/skybot/commands/uncategorized/IssueCommand.kt
@@ -20,4 +20,26 @@
 
 package ml.duncte123.skybot.commands.uncategorized
 
+import ml.duncte123.skybot.Author
+import ml.duncte123.skybot.objects.command.Command
+import ml.duncte123.skybot.utils.MessageUtils
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
+
 @Author(nickname = "Sanduhr32", author = "Maurice R S")
+class IssueCommand : Command() {
+    override fun executeCommand(invoke: String, args: Array<out String>, event: GuildMessageReceivedEvent) {
+        when (args.size) {
+            0 -> {
+                MessageUtils.sendErrorWithMessage(event.message, """Well you forgot to add formatted data we require so we can resolve it faster.
+                    |You can generate it by using our dashboard. Link: <https://bot.duncte123.me>""".trimMargin())
+            }
+        }
+    }
+
+    override fun help(): String = """Reports heavy and wierd issues to the developers.
+        |This will create an invite to your server, so we can join and help you directly.
+        |Those issues are hard to explain / resolve if we can't see nor read the chat other things that happen.
+    """.trimMargin()
+
+    override fun getName(): String = "issue"
+}
