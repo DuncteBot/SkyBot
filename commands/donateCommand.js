25af6d08b68106286c454b87cb7ccf769aa566e2
Duncan Sterken <duncan.sterken123@gmail.com>
2018-03-25 15:27:05 +0200

    test

--- commands/donateCommand.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    invoke: "donate", //No spaces
-    run: function (invoke, args, message, prefix) {
-        if(!args[0]){ message.channel.send("Here is my donation link:\n<https://paypal.me/duncte123>"); return;}
-        message.channel.send("Here is my donation link:\n<https://paypal.me/duncte123/" + args[0] + "");
-    },
-    help: "sends me some money"
-};
--- utils/settingsUtils.js
+++
@@ -1,5 +0,0 @@
-const prefixes = {};
-
-module.exports = {
-    prefixes: prefixes
-}
