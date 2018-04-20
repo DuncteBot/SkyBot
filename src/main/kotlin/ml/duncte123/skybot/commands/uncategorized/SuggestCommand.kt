86a1354d448ce2976cb6e95bc0936e0555f91386
Duncan Sterken <duncan.sterken123@gmail.com>
2018-04-20 17:04:44 +0200

    For real tho, this can be a oneliner

--- src/main/kotlin/ml/duncte123/skybot/commands/uncategorized/SuggestCommand.kt
+++ src/main/kotlin/ml/duncte123/skybot/commands/uncategorized/SuggestCommand.kt
@@ -1,25 +1,13 @@
 package ml.duncte123.skybot.commands.uncategorized
 
 import ml.duncte123.skybot.objects.command.Command
-import ml.duncte123.skybot.utils.AirUtils
-import ml.duncte123.skybot.utils.EmbedUtils
+import ml.duncte123.skybot.utils.MessageUtils
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 
 class SuggestCommand: Command() {
 
-    val suggestMessage = AirUtils.CONFIG.get("messages.suggest")
-            ?: """|Put your suggestions at [our Trello board](https://trello.com/b/iSaxpcGR/skybot-suggestions "Suggest to Dunctebot here!"
-            |Or use our [dashboard suggestion site](https://bot.duncte123.me/suggest.py).""".trimMargin()
-                
-
     override fun executeCommand(invoke: String, args: Array<out String>, event: GuildMessageReceivedEvent) {
-        var embed = EmbedUtils.defaultEmbed().setDscription(suggestMessage).build()
-
-        event.author.openPrivateChannel().queue {
-            it.sendMessage(embed).queue({}, {
-                event.channel.sendMessage(embed).queue()
-            })
-        }
+        MessageUtils.sendMsg(event, "You can leave suggestions for the bot on his page: https://bot.duncte123.me/suggest.html")
     }
 
     override fun getName() = "suggest"
