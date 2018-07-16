528c45ffb865862a7df4273e3e7a771896813f31
Duncan Sterken <duncan.sterken123@gmail.com>
2018-07-16 12:04:26 +0200

    Switch to other cat command if first one borks

--- src/main/java/ml/duncte123/skybot/commands/animals/CatCommand.java
+++ src/main/java/ml/duncte123/skybot/commands/animals/CatCommand.java
@@ -22,6 +22,7 @@ import me.duncte123.botCommons.web.WebUtils;
 import ml.duncte123.skybot.objects.command.Command;
 import ml.duncte123.skybot.objects.command.CommandCategory;
 import ml.duncte123.skybot.unstable.utils.ComparatingUtils;
+import ml.duncte123.skybot.utils.AirUtils;
 import ml.duncte123.skybot.utils.EmbedUtils;
 import ml.duncte123.skybot.utils.MessageUtils;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
@@ -53,7 +54,7 @@ public class CatCommand extends Command {
                     }
                 },
                 (error) -> {
-                    MessageUtils.sendMsg(event, "This command broke again *sigh*\nuse `" + PREFIX + "kitty` because it is more stable");
+                    AirUtils.COMMAND_MANAGER.dispatchCommand("kitty", args, event);
                     ComparatingUtils.execCheck(error);
                 }
         );
