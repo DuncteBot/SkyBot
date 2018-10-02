929b6be9151beb462b8ac0b9916f0a88e2cd9a69
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-10-02 19:58:45 +0530

    Update SkyBot.java

--- src/main/java/ml/duncte123/skybot/SkyBot.java
+++ src/main/java/ml/duncte123/skybot/SkyBot.java
@@ -213,7 +213,7 @@ public class SkyBot {
         GsonBuilder builder = new Gson().newBuilder().setPrettyPrinting().serializeNulls();
         String json = builder.create().toJson(config);
         try {
-            FileUtils.writeStringToFile(new File("config-empty.json"), json, Charset.forName("UTF-8"));
+            FileUtils.writeStringToFile(new File("config-empty.json"), json, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
