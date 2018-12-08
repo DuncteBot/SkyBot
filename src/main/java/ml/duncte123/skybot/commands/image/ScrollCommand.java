81d2c891cc420df84068c39771a6fde4e2d9351a
Duncan Sterken <duncan.sterken123@gmail.com>
2018-12-08 10:25:05 +0100

    Add db!purgechannel (#148)
    * Update kotlin
    
    * Prepare scroll
    
    * Lol abstraction
    
    * Add more filter commands
    
    * Create db!purgechannel
    
    * Add to help clear help
    
    * Illuminate strings
    
    * Remove unused methods

--- src/main/java/ml/duncte123/skybot/commands/image/ScrollCommand.java
+++ src/main/java/ml/duncte123/skybot/commands/image/ScrollCommand.java
@@ -16,21 +16,29 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-@file:Author(nickname = "duncte123", author = "Duncan Sterken")
+package ml.duncte123.skybot.commands.image;
 
-package ml.duncte123.skybot.objects.api
+import ml.duncte123.skybot.Settings;
+import ml.duncte123.skybot.objects.command.CommandContext;
+import org.jetbrains.annotations.NotNull;
 
-import ml.duncte123.skybot.Author
-import java.sql.Date
+public class ScrollCommand extends ImageCommandBase {
+    @Override
+    public void executeCommand(@NotNull CommandContext ctx) {
+        if (!passes(ctx.getEvent(), ctx.getArgs(), true)) return;
 
-data class LlamaObject(val id: Int, val file: String)
+        ctx.getAlexFlipnote().getScroll(parseTextArgsForImage(ctx))
+            .async((image) -> handleBasicImage(ctx.getEvent(), image));
+    }
 
-data class AlpacaObject(val file: String)
+    @Override
+    public String getName() {
+        return "scroll";
+    }
 
-data class KpopObject(val id: Int, val name: String, val band: String, val image: String)
-
-data class WarnObject(val userId: String, val warnings: List<Warning>)
-data class Warning(val id: Int, val date: Date, val expiryDate: Date, val modId: String, val reason: String, val guildId: String)
-
-data class Ban(val id: Int, val modId: String, val userId: String, val userName: String, val discriminator: String, val guildId: String)
-
+    @Override
+    public String help() {
+        return "The scroll of truth\n" +
+            "Usage: `" + Settings.PREFIX + getName() + " <text>`";
+    }
+}
--- src/main/java/ml/duncte123/skybot/commands/image/filter/InvertCommand.java
+++ src/main/java/ml/duncte123/skybot/commands/image/filter/InvertCommand.java
@@ -16,21 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-@file:Author(nickname = "duncte123", author = "Duncan Sterken")
+package ml.duncte123.skybot.commands.image.filter;
 
-package ml.duncte123.skybot.objects.api
-
-import ml.duncte123.skybot.Author
-import java.sql.Date
-
-data class LlamaObject(val id: Int, val file: String)
-
-data class AlpacaObject(val file: String)
-
-data class KpopObject(val id: Int, val name: String, val band: String, val image: String)
-
-data class WarnObject(val userId: String, val warnings: List<Warning>)
-data class Warning(val id: Int, val date: Date, val expiryDate: Date, val modId: String, val reason: String, val guildId: String)
-
-data class Ban(val id: Int, val modId: String, val userId: String, val userName: String, val discriminator: String, val guildId: String)
-
+public class InvertCommand extends FilterBase {
+    // The FilterBase class handles everything
+}
