beeb61f1403a1ae045532d06ef5d8183ada3d38d
duncte123 <duncan.sterken123@gmail.com>
2019-07-25 16:34:10 +0200

    Add test flag command

---
+++ src/main/java/ml/duncte123/skybot/commands/uncategorized/TestFlagCommand.java
@@ -0,0 +1,47 @@
+/*
+ * Skybot, a multipurpose discord bot
+ *      Copyright (C) 2017 - 2019  Duncan "duncte123" Sterken & Ramid "ramidzkh" Khan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ml.duncte123.skybot.commands.uncategorized;
+
+import ml.duncte123.skybot.objects.command.Command;
+import ml.duncte123.skybot.objects.command.CommandContext;
+import ml.duncte123.skybot.objects.command.Flag;
+
+import javax.annotation.Nonnull;
+
+public class TestFlagCommand extends Command {
+
+    public TestFlagCommand() {
+        this.name = "flag";
+        this.flags = new Flag[] {
+          new Flag(
+              't',
+              "first test flag"
+          ),
+          new Flag(
+              "test",
+              "second test flag"
+          ),
+        };
+    }
+
+    @Override
+    public void execute(@Nonnull CommandContext ctx) {
+        //
+    }
+}
