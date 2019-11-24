a069a351ef2bfc9235374ee4b1d83b2154619544
Duncan Sterken <duncan.sterken123@gmail.com>
2019-11-24 13:27:05 +0100

    Update JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     implementation(group = "me.duncte123", name = "botCommons", version = "1.0.58")
 
     // JDA (java discord api)
-    implementation(group = "net.dv8tion", name = "JDA", version = "4.0.0_62") {
+    implementation(group = "net.dv8tion", name = "JDA", version = "4.0.0_63") {
         exclude(module = "opus-java")
     }
 
