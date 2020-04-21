a07b8cbccc06a9420bae65bd2fd28c32c41a9f01
duncte123 <duncan.sterken123@gmail.com>
2020-04-21 19:31:29 +0200

    Cleanup some radio stations

--- src/main/kotlin/ml/duncte123/skybot/commands/music/RadioCommand.kt
+++ src/main/kotlin/ml/duncte123/skybot/commands/music/RadioCommand.kt
@@ -22,20 +22,18 @@ import me.duncte123.botcommons.messaging.EmbedUtils
 import me.duncte123.botcommons.messaging.MessageUtils.*
 import ml.duncte123.skybot.Author
 import ml.duncte123.skybot.SinceSkybot
-import ml.duncte123.skybot.objects.ILoveStream
 import ml.duncte123.skybot.objects.RadioStream
 import ml.duncte123.skybot.objects.command.CommandContext
 import ml.duncte123.skybot.objects.command.MusicCommand
 import net.dv8tion.jda.api.MessageBuilder
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.utils.data.DataArray
-import net.dv8tion.jda.api.utils.data.DataObject
 
 @Author(nickname = "Sanduhr32", author = "Maurice R S")
 @SinceSkybot("3.52.2")
 class RadioCommand : MusicCommand() {
 
-    var radioStreams: ArrayList<RadioStream> = ArrayList()
+    var radioStreams = arrayListOf<RadioStream>()
 
     init {
         this.withAutoJoin = true
@@ -43,7 +41,6 @@ class RadioCommand : MusicCommand() {
         this.aliases = arrayOf("pstream", "stream", "webstream", "webradio")
         this.help = "Adds a radio http stream to your queue and goes to it"
         this.usage = "<(full)list/station name>"
-        // TODO: load from json
         loadStations()
     }
 
@@ -64,6 +61,7 @@ class RadioCommand : MusicCommand() {
                         sendRadioSender(event = event)
                         return
                     }
+                    // TODO: send a link to the website or something, this is too huge
                     "fulllist" -> {
                         sendRadioSender(event = event, full = true)
                         return
@@ -90,54 +88,54 @@ class RadioCommand : MusicCommand() {
     }
 
     private fun sendRadioSender(event: GuildMessageReceivedEvent, full: Boolean = false) {
-        val streams = radioStreams
-        val string = streams.filter { if (!full) it.listed else true }
-            .joinToString(separator = "\n") { it.toEmbedString() }
-        for (it in MessageBuilder().append(string).buildAll(MessageBuilder.SplitPolicy.NEWLINE)) {
-            sendEmbed(event, EmbedUtils.defaultEmbed().setDescription(it.contentRaw).build())
+        val selectedStreams = if (full) {
+            radioStreams
+        } else {
+            val out = arrayListOf<RadioStream>()
+
+            repeat(5) {
+                var selected = radioStreams.random()
+
+                while (out.contains(selected)) {
+                    selected = radioStreams.random()
+                }
+
+                out.add(selected)
+            }
+
+            out
         }
+        val string = selectedStreams.joinToString(separator = "\n") { it.toEmbedString() }
+
+        MessageBuilder()
+            .append(string)
+            .buildAll(MessageBuilder.SplitPolicy.NEWLINE)
+            .forEachIndexed { index, it ->
+                val embed = EmbedUtils.embedMessage(it.contentRaw)
+
+                if (!full && index == 0) {
+                    embed.setTitle("Here are 5 random entries from our radio list")
+                }
+
+                sendEmbed(event, embed)
+            }
     }
 
     private fun loadStations() {
-        //Sorting via locales https://lh.2xlibre.net/locales/
+        val streams = this.javaClass.getResource("/radio_streams.json").readText()
+        val json = DataArray.fromJson(streams)
 
-        //de_DE radio stations
-//        radioStreams.add(RadioStream("iloveradio", "http://stream01.iloveradio.de/iloveradio1.mp3", "http://www.iloveradio.de/streams/"))
-        // TODO: why so many
-        // TODO: Make a json file or api endpoint with all streams for easy updating
+        json.forEach {
+            it as HashMap<*, *>
 
-        radioStreams.add(ILoveStream(stationName = "iloveradio", channel = 1))
-        radioStreams.add(ILoveStream(stationName = "ilove2dance", channel = 2))
-        radioStreams.add(ILoveStream(stationName = "ilovetop100charts", channel = 9))
-        radioStreams.add(ILoveStream(stationName = "ilovethebattle", channel = 3, npChannel = 4, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovebass", channel = 4, npChannel = 3, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovemashup", channel = 5, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovedreist", channel = 6, public = false))
-        radioStreams.add(ILoveStream(stationName = "iloveberlin", channel = 7, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovexmas", channel = 8, public = false))
-        radioStreams.add(ILoveStream(stationName = "iloveandchill", channel = 10, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovetop100dance&dj", channel = 103, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovetop100pop", channel = 105, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovetop100hiphop", channel = 108, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovepopstars", channel = 11, npChannel = 16, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovehistory", channel = 12, npChannel = 15, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovehiphop", channel = 13, npChannel = 17, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovethesun", channel = 15, npChannel = 19))
-        radioStreams.add(ILoveStream(stationName = "iloveurban", channel = -1, npChannel = 12, internal = false, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovegroove", channel = -1, npChannel = 13, internal = false, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovenitroxedm", channel = -1, npChannel = 11, internal = false, public = false))
-        radioStreams.add(ILoveStream(stationName = "ilovenitroxdeep", channel = -1, npChannel = 24, internal = false, public = false))
-
-        //nl_NL radio stations
-        radioStreams.add(RadioStream("slam", "https://playerservices.streamtheworld.com/api/livestream-redirect/SLAM_MP3_SC", "https://live.slam.nl/slam-live/"))
-        radioStreams.add(RadioStream("radio538", "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO538.mp3", "https://www.538.nl/"))
-        radioStreams.add(RadioStream("3fm", "https://icecast.omroep.nl/3fm-sb-mp3", "https://www.npo3fm.nl/", false))
-        radioStreams.add(RadioStream("skyradio", "https://playerservices.streamtheworld.com/api/livestream-redirect/SKYRADIO_SC", "http://www.skyradio.nl/", false))
-        radioStreams.add(RadioStream("qmusic", "https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_live_96.mp3", "http://qmusic.nl/", false))
-
-        //International radio stations
-        //TODO: add international radio stations
-        radioStreams.add(RadioStream("trapfm", "http://stream.trap.fm:6004/;stream.mp3", "https://trap.fm/"))
-        radioStreams.add(RadioStream("listen.moe", "https://listen.moe/stream", "https://listen.moe/stream"))
+            radioStreams.add(
+                RadioStream(
+                    name = it["name"].toString(),
+                    url = it["audio"].toString(),
+                    website = it["website"].toString(),
+                    listed = false
+                )
+            )
+        }
     }
 }
