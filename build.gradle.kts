86282378ecbe12e2879e03a31675a97c6edd7895
duncte123 <contact@duncte123.me>
2020-06-01 11:36:12 +0200

    Bumb the bot's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ plugins {
     id("com.github.breadmoirai.github-release") version "2.2.12"
 }
 
-val numberVersion = "3.92.0"
+val numberVersion = "3.92.1"
 
 project.group = "ml.duncte123.skybot"
 project.version = "${numberVersion}_${getGitHash()}"
