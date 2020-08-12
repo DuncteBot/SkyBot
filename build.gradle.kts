ea44bf468c982449621b5a300c542156edc4a4e8
duncte123 <contact@duncte123.me>
2020-08-12 09:46:11 +0200

    Missed a settings command a while ago

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
     implementation(group = "me.duncte123", name = "botCommons", version = "BETA_2.0.107")
 
     // JDA (java discord api)
-    implementation(group = "net.dv8tion", name = "JDA", version = "4.2.0_184") {
+    implementation(group = "net.dv8tion", name = "JDA", version = "4.2.0_189") {
         exclude(module = "opus-java")
     }
 
