58f00906b88d67873fd4c28d1d384f48767b7f44
duncte123 <contact@duncte123.me>
2020-10-02 09:19:46 +0200

    Start on apple music support

---
+++ src/main/java/ml/duncte123/skybot/audio/sourcemanagers/apple/AppleMusicAudioSourceManager.java
@@ -0,0 +1,62 @@
+/*
+ * Skybot, a multipurpose discord bot
+ *      Copyright (C) 2017 - 2020  Duncan "duncte123" Sterken & Ramid "ramidzkh" Khan & Maurice R S "Sanduhr32"
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ml.duncte123.skybot.audio.sourcemanagers.apple;
+
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioItem;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AppleMusicAudioSourceManager implements AudioSourceManager {
+    @Override
+    public String getSourceName() {
+        return "apple_music";
+    }
+
+    @Override
+    public AudioItem loadItem(DefaultAudioPlayerManager manager, AudioReference reference) {
+        return null;
+    }
+
+    @Override
+    public boolean isTrackEncodable(AudioTrack track) {
+        return false;
+    }
+
+    @Override
+    public void encodeTrack(AudioTrack track, DataOutput output) throws IOException {
+
+    }
+
+    @Override
+    public AudioTrack decodeTrack(AudioTrackInfo trackInfo, DataInput input) throws IOException {
+        return null;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
