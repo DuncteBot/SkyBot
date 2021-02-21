477e090150f05a514e84aca19035b1ef3369bbf3
duncte123 <contact@duncte123.me>
2021-02-21 13:31:34 +0100

    Add playback support for tiktok

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,7 +102,8 @@ dependencies {
         exclude(module = "opus-java")
     }*/
 
-    implementation(group = "com.dunctebot", name = "sourcemanagers", version = "1.5.3")
+//    implementation(group = "com.dunctebot", name = "sourcemanagers", version = "1.5.3")
+    implementation(group = "com.github.DuncteBot", name = "skybot-source-managers", version = "aad4b94")
     // Lavaplayer/Lavalink
     implementation(group = "com.sedmelluq", name = "lavaplayer", version = "1.3.67")
     implementation(group = "com.github.DuncteBot", name = "Lavalink-Client", version = "d2fb620")
