c19359530f54b97bfa9d790edc5357c146277831
duncte123 <contact@duncte123.me>
2021-04-10 17:40:15 +0200

    Allow for the duration format in the slowmode command

--- src/main/kotlin/ml/duncte123/skybot/commands/mod/SlowModeCommand.kt
+++ src/main/kotlin/ml/duncte123/skybot/commands/mod/SlowModeCommand.kt
@@ -20,6 +20,7 @@ package ml.duncte123.skybot.commands.mod
 
 import me.duncte123.botcommons.messaging.MessageUtils.sendMsg
 import me.duncte123.botcommons.messaging.MessageUtils.sendSuccess
+import me.duncte123.durationparser.DurationParser
 import ml.duncte123.skybot.commands.guild.mod.ModBaseCommand
 import ml.duncte123.skybot.objects.command.CommandContext
 import ml.duncte123.skybot.utils.AirUtils
@@ -54,19 +55,25 @@ class SlowModeCommand : ModBaseCommand() {
             return
         }
 
-        if (!AirUtils.isInt(delay)) {
-            sendMsg(ctx, "Provided argument is not an integer")
-            return
-        }
+        val intDelay = if (!AirUtils.isInt(delay)) {
+            val parser = DurationParser.parse(delay)
 
-        val intDelay = delay.toInt()
+            if (parser.isEmpty) {
+                sendMsg(ctx, "Provided argument is not an integer or duration")
+                return
+            }
+
+            parser.get().seconds
+        } else {
+            delay.toLong()
+        }
 
         if (intDelay < 0 || intDelay > TextChannel.MAX_SLOWMODE) {
             sendMsg(ctx, "$intDelay is not valid, a valid delay is a number in the range 0-${TextChannel.MAX_SLOWMODE} (21600 is 6 hours in seconds)")
             return
         }
 
-        ctx.channel.manager.setSlowmode(intDelay).reason("Requested by ${ctx.author.asTag}").queue()
+        ctx.channel.manager.setSlowmode(intDelay.toInt()).reason("Requested by ${ctx.author.asTag}").queue()
         sendSuccess(ctx.message)
     }
 }
