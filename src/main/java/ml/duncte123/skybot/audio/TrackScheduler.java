997a15510bc579b1981964a7dd7c63fd92e669c4
duncte123 <contact@duncte123.me>
2021-08-06 08:19:49 +0200

    Lint code

--- src/main/java/ml/duncte123/skybot/audio/TrackScheduler.java
+++ src/main/java/ml/duncte123/skybot/audio/TrackScheduler.java
@@ -136,7 +136,7 @@ public class TrackScheduler extends AudioEventAdapterWrapped {
         final TrackUserData data = track.getUserData(TrackUserData.class);
 
         // If the track was a skipped track or we announce tracks
-        if ((data != null && data.getWasFromSkip()) || this.guildMusicManager.isAnnounceTracks()) {
+        if (data != null && data.getWasFromSkip() || this.guildMusicManager.isAnnounceTracks()) {
             // Reset the was from skip status
             if (data != null) {
                 data.setWasFromSkip(false);
