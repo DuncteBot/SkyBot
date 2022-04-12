82d039dcafac8abd3b4d1d7aff5ad2a7915e5af1
duncte123 <contact@duncte123.me>
2022-04-12 11:34:59 +0200

    Prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
     id("org.jmailen.kotlinter") version "3.5.1"
 }
 
-val numberVersion = "3.106.6"
+val numberVersion = "3.106.7"
 
 project.group = "ml.duncte123.skybot"
 project.version = "${numberVersion}_${getGitHash()}"
