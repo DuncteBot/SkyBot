5f4b8ac0f75d38ad4581aab5c30b0379506e2138
duncte123 <contact@duncte123.me>
2024-02-08 18:01:09 +0100

    Release 3.109.1

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -36,7 +36,7 @@ plugins {
 
 val pmdVersion = "7.0.0-rc4"
 
-val numberVersion = "3.109.0"
+val numberVersion = "3.109.1"
 
 project.group = "me.duncte123.skybot"
 project.version = "${numberVersion}_${getGitHash()}"
