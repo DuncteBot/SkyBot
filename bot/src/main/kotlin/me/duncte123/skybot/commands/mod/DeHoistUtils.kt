eef1e00057a4b48bfe24aa3d1c8ecb80eb784b99
duncte123 <contact@duncte123.me>
2024-06-24 18:46:47 +0200

    Move dehoist command to slash support

--- bot/src/main/kotlin/me/duncte123/skybot/commands/mod/DeHoistUtils.kt
+++ bot/src/main/kotlin/me/duncte123/skybot/commands/mod/DeHoistUtils.kt
@@ -22,6 +22,7 @@ import me.duncte123.botcommons.messaging.MessageUtils.sendMsg
 import me.duncte123.botcommons.messaging.MessageUtils.sendSuccess
 import me.duncte123.skybot.Variables
 import me.duncte123.skybot.commands.guild.mod.ModBaseCommand
+import me.duncte123.skybot.entities.jda.DunctebotGuild
 import me.duncte123.skybot.objects.command.CommandContext
 import me.duncte123.skybot.utils.FinderUtils
 import me.duncte123.skybot.utils.GuildSettingsUtils
@@ -29,7 +30,11 @@ import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameEvent
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 import java.lang.Character.UnicodeBlock
 import java.text.Normalizer
 import kotlin.random.Random
@@ -138,6 +143,67 @@ class DeHoistCommand : ModBaseCommand() {
         ctx.guild.modifyNickname(toDehoist, toDehoist.cleanedDisplayName)
             .reason("de-hoist/nickname cleaning ${ctx.author.asTag}").queue()
         sendSuccess(ctx.message)
+    }
+
+    override fun configureSlashSupport(baseData: SlashCommandData) {
+        baseData.addOptions(
+            OptionData(
+                OptionType.USER,
+                "user",
+                "The user to dehoist, omit for checking everyone",
+                false
+            )
+        )
+    }
+
+    override fun handleEvent(event: SlashCommandInteractionEvent, guild: DunctebotGuild, variables: Variables) {
+        val userOption = event.getOption("user")
+        val selfMember = guild.selfMember
+
+        if (userOption == null) {
+            // DeHoist all
+            event.deferReply().queue()
+            guild.loadMembers {
+                if (selfMember.canInteract(it) && shouldDehoist(it)) {
+                    it.modifyNickname(it.cleanedDisplayName)
+                        .reason("de-hoist/nickname cleaning by ${event.user.asTag}")
+                        .queue()
+                }
+            }.onSuccess {
+                event.hook.editOriginal("Cleaning complete!").queue()
+            }
+            return
+        }
+
+        val toDehoist = userOption.asMember
+
+        if (toDehoist == null) {
+            event.reply("Could not find user for your input")
+                .setEphemeral(true)
+                .queue()
+            return
+        }
+
+        val moderator = event.member!!
+
+        if (!selfMember.canInteract(toDehoist)) {
+            event.reply("I cannot alter that user")
+                .setEphemeral(true)
+                .queue()
+            return
+        }
+
+        if (!moderator.canInteract(toDehoist)) {
+            event.reply("You cannot change the nickname of that user")
+                .setEphemeral(true)
+                .queue()
+            return
+        }
+
+        guild.modifyNickname(toDehoist, toDehoist.cleanedDisplayName)
+            .reason("de-hoist/nickname cleaning ${event.user.asTag}").queue()
+
+        event.reply("Dehoisted ${event.user.asTag}").queue()
     }
 }
 
